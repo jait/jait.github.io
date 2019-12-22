90529bbdab25639d5c52f39f563523a6801c6fee
Jari Tenhunen <jari.tenhunen@boogiesoftware.com>
2019-12-23 01:03:42 +0200

    Some shortcuts

--- x/x.js
+++ x/x.js
@@ -9,14 +9,12 @@ function homma() {
 		var dx = 50 + y/2.5;
 		var dy = 50 + y/1.5;
 		var p = new Path2D(`M ${x-dx} ${y+dy} C ${x-dx/2} ${y+dy}, ${x} ${y+dy/2}, ${x} ${y} ` +
-			    `C ${x} ${y+dy/2}, ${x+dx/2} ${y+dy}, ${x+dx} ${y+dy} ` +
-			    `L ${x-dx} ${y+dy}`
-			    );
+			`C ${x} ${y+dy/2}, ${x+dx/2} ${y+dy}, ${x+dx} ${y+dy} Z`);
 		ctx.fill(p);
 	}
 
 	function b(by) {
-		ctx.fill(new Path2D(`M ${x-15} ${by} L ${x+15} ${by} L ${x+10} ${by-40} L ${x-10} ${by-40}`));
+		ctx.fill(new Path2D(`M ${x-15} ${by} h 30 L ${x+10} ${by-40} h -20 Z`));
 	}
 
 	ctx.fillStyle = "#662211";
