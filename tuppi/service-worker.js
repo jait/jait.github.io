4f4637d48c8f76a64ac7cf9046c960c5673759e6
Jari Tenhunen <jari.tenhunen@boogiesoftware.com>
2020-04-25 11:09:02 +0300

    Update with relative path

--- tuppi/service-worker.js
+++ tuppi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b80be02e73ccc9576cca3bc87761d529.js"
+  "./precache-manifest.3ba549866201394dcc999bc22bb73809.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
